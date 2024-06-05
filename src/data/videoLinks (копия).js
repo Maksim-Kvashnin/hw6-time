f84fe9aca8ddf263fd3725c65c34ebfd651c130e
Maksim_KA <Kvashnin@hotmail.com>
2024-06-05 23:09:15 +0700

    done

---
+++ src/data/videoLinks (копия).js
@@ -0,0 +1,28 @@
+const videoLinks = [
+  {
+    id: 'mzdzMJthhVOJWVgAWsBOV',
+    url: 'https://www.youtube.com/embed/iCZm61G5zOc?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2024-06-05 04:24:00',
+    title: 'Cat TV for Cats to Watch 🐈 - THE BIRD PARLIAMENT 🐦‍⬛(4K)',
+  },
+  {
+    id: '6qaRTOdEZbzYJfTMJm6DM',
+    url: 'https://www.youtube.com/embed/mhhAmH5gBOk?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2023-08-21 12:18:00',
+    title: 'Шум моря 🐢 музыка, расслабляющая водных животных 🐋',
+  },
+  {
+    id: 'G5RdV1pOqHALXLT8hY1Oa',
+    url: 'https://www.youtube.com/embed/GQvHm9Cz94A?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2022-10-03 09:43:00',
+    title: 'Amazon 4k — крупнейший в мире тропический лес, часть 3 | Расслабляющий фильм о природе',
+  },
+  {
+    id: 'QXxWn9sZvGkX7gYSwSbg4',
+    url: 'https://www.youtube.com/embed/gTo495m8l6E?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2022-09-23 21:06:00',
+    title: 'Мир ПТИЦ в 4K - Исцеляющая сила птичьих звуков | Сценический релаксационный фильм',
+  },
+]
+
+export default videoLinks;
---
+++ src/data/videoLinks.js
@@ -0,0 +1,40 @@
+const contentLinks = [
+  {
+    id: 'mzdzMJthhVOJWVgAWsBOV',
+    url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2017-07-31 13:24:00',
+    title: 'Самые красивые цвета природы в 4K 🌻🐦Птицы и цветы.',
+  },
+  {
+    id: '6qaRTOdEZbzYJfTMJm6DM',
+    url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2018-03-03 12:10:00',
+    title: 'лучший расслабляющий аквариум в 4K UHD 🐠',
+  },
+  {
+    id: 'QXxWn9sZvGkX7gYSwSbg4',
+    url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2018-02-03 23:16:00',
+    title: '4K Tropical Beach - Relaxing Sea/ Ocean Wave Sounds',
+  },
+  {
+    id: '6qaRTOdEZbzYJfTMJm6DM',
+    url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2018-01-03 12:10:00',
+    title: 'Самые красивые цвета природы в 4K III 🐦 Разноцветные животные.',
+  },
+  {
+    id: 'G5RdV1pOqHALXLT8hY1Oa',
+    url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2018-01-01 16:17:00',
+    title: '4K Ultra HD Видео о диких животных',
+  },
+  {
+    id: 'SMDTTcsKWo6P0uyxghX9T',
+    url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+    date: '2017-12-02 05:24:00',
+    title: 'Африканская дикая природа 4K Ultra HD',
+  },
+]
+
+export default contentLinks;
